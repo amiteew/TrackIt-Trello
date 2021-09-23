a00371d36b9f053da61fce96e36e56d81259bcfe
Amitee Weiner <amitweiner@google.com>
2021-09-23 18:42:13 +0300

    board header change title

--- frontend/src/cmp/BoardHeader.jsx
+++ frontend/src/cmp/BoardHeader.jsx
@@ -1,52 +1,72 @@
-import React from 'react'
-import { connect } from 'react-redux'
-
+import React from "react"
+import { connect } from "react-redux"
 
 class _BoardHeader extends React.Component {
+  state = {
+    isEditTitle: false,
+    title: ''
+  }
 
-    state = {
-        boardTitle: ""
+  componentDidMount() {
+    // this.props.loadUsers();
+    // this.props.loadBoard();
+  }
 
-    }
-    componentDidMount() {
-        this.props.loadUsers();
-        this.props.loadBoard();
-    }
+  handleChange = (ev) => {
+    const value = ev.target.value
+    this.setState((prevState) => ({ ...prevState, title: value }))
+  }
 
-    render() {
-        const { boardTitle: boardName } = this.state;
+  toggleChangeTitle = () => {
+    const { isEditTitle } = this.state
+    this.setState((prevState) => ({ ...prevState, isEditTitle: !isEditTitle }))
+  }
 
-        return (
-            <div className="board-Header">
-                <h1>
-                    <input
-                        name='boardName'
-                        id='boardName'
-                        type='text'
-                        placeholder='boardName'
-                        value={boardName}
-                        onChange={this.handleChange}
-                    />
+  onSaveBoardTitle = () => {
+    ev.preventDefault()
+    const { title } = this.state
+    if (!title) return
+    const { board, onUpdateBoard } = this.props
+    board.boardTitle = title
+    onUpdateBoard(board)
+    toggleChangeTitle()
+  }
 
-                </h1>
+  render() {
+    const { board } = this.props
+    const { isEditTitle } = this.state
 
-            </div>
-        )
-    }
+    return (
+      <div className="board-header">
+        {!isEditTitle && <h1 onClick={this.toggleChangeTitle}>{board.boardTitle}</h1>}
+        {isEditTitle &&
+          <form onSubmit={this.onSaveBoardTitle}>
+            <input
+              name="boardTitle"
+              id="boardTitle"
+              type="text"
+              placeholder="Enter Title"
+              value={board.boardTitle}
+              onChange={this.handleChange}
+            />
+          </form>
+        }
+
+      </div>
+    )
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        users: state.userModule.users,
-        user: state.userModule.user,
-        boards: state.boardModule.boards
-    }
+  return {
+    users: state.userModule.users,
+    user: state.userModule.user,
+    boards: state.boardModule.boards,
+  }
 }
 const mapDispatchToProps = {
-    loadUsers,
-    loadBoards
+  loadUsers,
+  loadBoards,
 }
-
-
 
 export const BoardHeader = connect(mapStateToProps, mapDispatchToProps)(_BoardHeader)
