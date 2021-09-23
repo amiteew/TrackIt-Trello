f48db178debdfb07a11a06cc7c895d309290f366
Amitee Weiner <amitweiner@google.com>
2021-09-23 19:35:51 +0300

    board header starred

--- frontend/src/cmp/BoardHeader.jsx
+++ frontend/src/cmp/BoardHeader.jsx
@@ -26,12 +26,19 @@ export class BoardHeader extends React.Component {
 
   saveBoardTitle = (ev) => {
     ev.preventDefault()
-    const { title } = this.state
-    if (!title) return
+    let { title } = this.state
     const { board, onUpdateBoard } = this.props
+    if (!title) title='Untitled'
     board.boardTitle = title
     onUpdateBoard(board)
     this.toggleChangeTitle()
+  }
+
+  onToggleStarBoard = () => {
+    const { board, onUpdateBoard } = this.props
+    const isStarred = board.isStarred
+    board.isStarred = !isStarred
+    onUpdateBoard(board)
   }
 
   render() {
@@ -39,7 +46,7 @@ export class BoardHeader extends React.Component {
     const { title, isEditTitle } = this.state
 
     return (
-      <div className="board-header">
+      <div className="board-header flex">
         {!isEditTitle && <h1 onClick={this.toggleChangeTitle}>{board.boardTitle}</h1>}
         {isEditTitle &&
           <form onSubmit={this.saveBoardTitle}>
@@ -54,6 +61,7 @@ export class BoardHeader extends React.Component {
             />
           </form>
         }
+        <button onClick={this.onToggleStarBoard}>{board.isStarred ? '🌟' : '⭐'}</button>
 
       </div>
     )
