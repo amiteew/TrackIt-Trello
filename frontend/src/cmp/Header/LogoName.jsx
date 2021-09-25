38964a46850dee6993988c0de21af38f8ac61167
Amitee Weiner <amitweiner@google.com>
2021-09-25 10:44:29 +0300

    get started as guest

--- frontend/src/cmp/Header/LogoName.jsx
+++ frontend/src/cmp/Header/LogoName.jsx
@@ -3,10 +3,9 @@ import ColorLogo from '../../assets/imgs/trackit-logo.svg'
 import WhiteLogo from '../../assets/imgs/trackit-logo-white.svg'
 
 export function LogoName({ isLoggedIn }) {
-    const linkSrc = isLoggedIn ? "/boards" : "/"
     const imgUrl = isLoggedIn ? WhiteLogo : ColorLogo
     return (
-        <NavLink to={linkSrc} >
+        <NavLink to="/" >
             <div className="logo-container flex align-center">
                 <span className="icon-span flex align-center justify-center"><img className="icon" src={imgUrl} alt="" /></span>
                 <h1 className="logo-name">TrackIt</h1>
