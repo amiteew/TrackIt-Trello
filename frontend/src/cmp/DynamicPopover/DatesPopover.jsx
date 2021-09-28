db810381d7d6078bab409a58adf5ff4db7af005b
Michael Uzan <looply3@gmail.com>
2021-09-28 18:09:39 +0300

    merged changes

--- frontend/src/cmp/DynamicPopover/DatesPopover.jsx
+++ frontend/src/cmp/DynamicPopover/DatesPopover.jsx
@@ -2,25 +2,8 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 import { loadBoards, removeBoard, addBoard, updateBoard } from '../../store/board.actions.js';
-<<<<<<< HEAD
-import DoneIcon from '@mui/icons-material/Done';
-import TextField from '@mui/material/TextField';
-// import { DatePicker } from '@mui/lab'
-// import StaticDatePicker from '@mui/lab/StaticDatePicker';
-// import LocalizationProvider from '@mui/lab/LocalizationProvider';
-// import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-// import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
-
-// import MomentUtils from '@date-io/moment';
-
-// import MomentUtils from '@date-io/moment';
-// import DateAdapter from '@mui/lab/AdapterDateFns';
-// import isWeekend from 'date-fns/isWeekend';
-// import isWeekend from '@date-io/date-fns/isWeekend';
-=======
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
->>>>>>> 19508876f5f439ac6d5c749f0310313c20e6d944
 
 class _DatesPopover extends React.Component {
     state = {
