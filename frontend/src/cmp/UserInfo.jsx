c04a8b0b267cb6ea2c8d3a785e6e6ec41fec38cc
Amitee Weiner <amitweiner@google.com>
2021-09-29 00:29:50 +0300

    make UserInfo function cmp and add scss

--- frontend/src/cmp/UserInfo.jsx
+++ frontend/src/cmp/UserInfo.jsx
@@ -1,30 +1,25 @@
-import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom';
-import { withRouter } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
 
-class _UserInfo extends React.Component {
-    closePopover = () => {
-        this.props.handleClose()
+function _UserInfo(props) {
+    const closePopover = () => {
+        props.handleClose()
     }
-    
-    render() {
-        const {user, loggedInUser} = this.props
-        return (
-            <div className="user-info flex">
-                <Avatar alt="" src={user.imgUrl} className="user-avatar">
-                    <p>{user.initials}</p>
-                </Avatar>
-                <div className="user-name-info">
-                    <p className="fullname">{user.fullname}</p>
-                    <p className="username">{user.username}</p>
-                    {/* {(loggedInUser._id === user._id) && <span className="edit-profile" onClick={this.onEditProfile}>Edit profile info</span>} */}
-                    {(loggedInUser._id === user._id) && <Link className="edit-profile" to="/boards" onClick={this.closePopover}>Edit profile info</Link>}
-                </div>
+    const { user, loggedInUser } = props
+
+    return (
+        <div className="user-info flex">
+            <Avatar alt="" src={user.imgUrl} className="user-avatar">
+                <p>{user.initials}</p>
+            </Avatar>
+            <div className="user-name-info">
+                <p className="fullname">{user.fullname}</p>
+                <p className="username">{user.username}</p>
+                {(loggedInUser._id === user._id) && <Link className="edit-profile" to="/boards" onClick={closePopover}>Edit profile info</Link>}
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 function mapStateToProps(state) {
@@ -33,5 +28,4 @@ function mapStateToProps(state) {
     }
 }
 
-const _UserInfoWithRouter = withRouter(_UserInfo);
-export const UserInfo = connect(mapStateToProps)(_UserInfoWithRouter)
+export const UserInfo = connect(mapStateToProps)(_UserInfo)
