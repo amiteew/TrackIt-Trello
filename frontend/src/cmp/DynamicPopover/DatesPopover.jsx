d8465204e8c86246be1bd3d8a95ee16d22c16a86
Michael Uzan <looply3@gmail.com>
2021-10-02 20:07:17 +0300

    working on dates in prigress

--- frontend/src/cmp/DynamicPopover/DatesPopover.jsx
+++ frontend/src/cmp/DynamicPopover/DatesPopover.jsx
@@ -1,8 +1,12 @@
+// import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+// import MomentUtils from '@date-io/moment';
 import React from 'react';
-
 import { connect } from 'react-redux';
 import { loadBoards, removeBoard, addBoard, updateBoard } from '../../store/board.actions.js';
+// REACT DATE PICKER NOT GOOD:
 import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+// METRIAL UI:
 
 class _DatesPopover extends React.Component {
     state = {
@@ -22,14 +26,38 @@ class _DatesPopover extends React.Component {
         console.log('im in DatesPopover.handleDateChange');
     }
 
+    handleDateSelect = (date) => {
+        console.log('date', date)
+    }
+
     render() {
         const { board, currListIdx, currCardIdx, startDate, setStartDate } = this.state
         if (!board || currCardIdx === null || currListIdx === null) return <></>
         const currCard = board.lists[currListIdx].cards[currCardIdx]
         return (
+
+            // <MuiPickersUtilsProvider utils={MomentUtils}>
+            //     <DatePicker
+            //         autoOk
+            //         variant="static"
+            //         openTo="date"
+            //         value={new Date()}
+            // onChange={this.handleChange}
+
+            //     />
+            // </MuiPickersUtilsProvider>
+
+            // REACT NOT GOOD:
+
             <DatePicker
                 selected={startDate}
-                onChange={(date) => setStartDate(date)}
+                // onChange={(date) => setStartDate(date)}
+                onSelect={(date) => this.handleDateSelect(date)}
+                inline
+                peekNextMonth
+                showMonthDropdown
+                showYearDropdown
+                dropdownMode="select"
             />
         )
     }
