a5a1937aa7e831e299f92cc456992290a030730f
Amitee Weiner <amitweiner@google.com>
2021-10-03 14:58:53 +0300

    board-menu css

--- frontend/src/cmp/DroweMenu.jsx
+++ frontend/src/cmp/DroweMenu.jsx
@@ -13,37 +13,53 @@ export function TemporaryDrawer({ board, toggleMenu, isMenuOpen }) {
         if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
             return;
         }
+        toggleMenu()
+        console.log('toggle menu');
     };
 
     const list = (anchor) => (
-        <div className="board-menu-content"
+        <div className="menu-wrapper">
+            <div className="menu-screen" onClick={toggleDrawer(anchor, false)}></div>
+            <div className="board-menu-content"
             // sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
             // role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
-        >
-            <div className="board-menu-content-header flex">
-                <div className="title"><h3>Menu</h3></div>
-                <span className="exit-icon" onClick={toggleMenu}>
-                    <img src={close} alt="Close" />
-                </span>
-            </div>
-            <Divider />
-            <div>
-                <ShowMenu board={board} />
+            // onClick={toggleDrawer(anchor, false)}
+            // onKeyDown={toggleDrawer(anchor, false)}
+            >
+                <div className="board-menu-content-header flex">
+                    <div className="title"><h3>Menu</h3></div>
+                    {/* <span className="exit-icon" onClick={toggleMenu}> */}
+                    <span className="exit-icon" onClick={toggleDrawer(anchor, false)}>
+                        <img src={close} alt="Close" />
+                    </span>
+                </div>
+                <Divider />
+
+                <div className="menu-content fancy-scrollbar">
+                    <div className="board-actions">
+                        <button className="menu-action-btn">
+                            <span className="change-bg-preview" style={board.boardStyle}></span>
+                            Change background
+                        </button>
+                    </div>
+                    <Divider />
+                    <div className="activities">
+                        <ShowMenu board={board} />
+                    </div>
+                </div>
             </div>
         </div>
     );
     const anchor = 'right';
 
     return (
-        <div>
+        <div className="menu-visible">
             <React.Fragment key={anchor}>
                 <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
                 <Drawer
                     anchor={anchor}
                     open={isMenuOpen}
-                    onClose={toggleDrawer(anchor, false)}
+                    // onClose={toggleDrawer(anchor, false)}
                     hideBackdrop={true}
                 >
                     {list(anchor)}
--- frontend/src/cmp/ShowMenu.jsx
+++ frontend/src/cmp/ShowMenu.jsx
@@ -1,12 +1,15 @@
+import Divider from '@mui/material/Divider';
 import { ActivityPreview } from './ActivityPreview.jsx';
 
 export function ShowMenu({ board }) {
 
-   return (
-       <div className="menu-content">
-           {board.activities.length && board.activities.map(activity => <ActivityPreview activity={activity} display={'menu'} key={activity.id} />)}
-       </div>
-   )
+    return (
+        <>
+            {/* <div className="menu-content"> */}
+                {board.activities.length && board.activities.map(activity => <ActivityPreview activity={activity} display={'menu'} key={activity.id} />)}
+            {/* </div> */}
+        </>
+    )
 
 }
 
