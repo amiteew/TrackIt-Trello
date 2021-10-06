35c3485bbbae8cd01c4f2c390b3deaffb92452ca
Amitee Weiner <amitweiner@google.com>
2021-10-06 17:06:59 +0300

    userBoardsPopover

--- frontend/src/cmp/DynamicPopover/UserBoardsPopover.jsx
+++ frontend/src/cmp/DynamicPopover/UserBoardsPopover.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { FiStar } from 'react-icons/fi';
 import { userService } from '../../services/user.service';
 import { utilService } from '../../services/util.service';
-import { loadBoards } from '../../store/board.actions'
+import { loadBoards, updateBoard } from '../../store/board.actions'
 
 class _UserBoardsPopover extends React.Component {
 
@@ -12,36 +13,70 @@ class _UserBoardsPopover extends React.Component {
         if (loggedInUser && !boards.length) await this.props.loadBoards(loggedInUser._id)
     }
 
+    toggleStar = (ev, board) => {
+        ev.stopPropagation()
+        const { loggedInUser, updateBoard } = this.props
+        const updatedBoard = userService.toggleStarBoard(board, loggedInUser._id)
+        updateBoard(updatedBoard)
+        console.log('updated star board');
+    }
+
     getBoardBtns = (boards) => {
+        console.log('this.props:', this.props);
         return boards.map(board => {
             const bgStyle = utilService.getFormattedBgStyle(board.boardStyle)
-            return <Link key={board._id} className="board-menu-btn flex align-center" to={`/boards/${board._id}`} onClick={() => this.props.handleClose()}>
-                <span className="board-bg" style={bgStyle}></span>
-                <p>{board.boardTitle}</p>
-            </Link >
+            return <div className="board-menu-btn flex align-center">
+                <Link key={board._id} className="flex align-center" to={`/boards/${board._id}`} onClick={() => this.props.handleClose()}>
+                    <span className="board-bg" style={bgStyle}></span>
+                    <p>{board.boardTitle}</p>
+                </Link >
+                {(this.props.type === 'starredBoards') ? <button className="star-board" onClick={(ev) => this.toggleStar(ev, board)}><FiStar /></button> : <></>}
+            </div>
+            // <button key={board._id} className="board-menu-btn flex align-center" onClick={() => this.props.goToBoard(board._id)}>
+            //      <span className="board-bg" style={bgStyle}></span>
+            //      <p>{board.boardTitle}</p>
+            //      {(this.props.type === 'starredBoards') ? <span onClick={(ev) => this.toggleStar(ev, board)}><FiStar /></span> : <></>}
+            //  </button>
         })
     }
 
-    render() {
+    getfilteredBoards = (type) => {
         const { boards, loggedInUser } = this.props
-        console.log('boards',boards);
-        const starredBoards = userService.filterUserBoards(boards, loggedInUser._id, "starred")
-        const nonStarredBoards = userService.filterUserBoards(boards, loggedInUser._id, "non-starred")
-        if (!starredBoards.length && !nonStarredBoards.length) return <div>No boards yet...</div>
+        return userService.filterUserBoards(boards, loggedInUser._id, type)
+    }
+
+    goToBoard = (boardId) => {
+        this.props.handleClose()
+        this.props.history.push(`/boards/${boardId}`)
+    }
+
+    render() {
+        const { type } = this.props
+        const boardsToShow = (type === 'userBoards') ? this.getfilteredBoards("owner") : this.getfilteredBoards("starred")
+        const guestBoards = (type === 'userBoards') ? this.getfilteredBoards("guest") : []
+        if (!boardsToShow.length && !guestBoards.length) return <div>No boards yet...</div>
         return (
             <div className="user-boards-popover">
-                {starredBoards.length ? <div className="starred-section">
-                    <h4>Starred Boards</h4>
+                {type === 'userBoards' && <>
+                    {boardsToShow.length ? <div className="owner-section">
+                        <h4>Your Boards</h4>
+                        <div className="owned-boards">
+                            {this.getBoardBtns(boardsToShow)}
+                        </div>
+                    </div> : <></>}
+                    {guestBoards.length ? <div className="guest-section">
+                        <h4>Guest Boards</h4>
+                        <div className="guest-boards">
+                            {this.getBoardBtns(guestBoards)}
+                        </div>
+                    </div> : <></>}
+                </>}
+                {type === 'starredBoards' ? <div className="starred-section">
                     <div className="starred-boards">
-                        {this.getBoardBtns(starredBoards)}
+                        {this.getBoardBtns(boardsToShow)}
                     </div>
-                </div> : <></>}
-                {nonStarredBoards.length ? <div className="starred-section">
-                    {starredBoards.length ? <h4>Other Boards</h4> : <></>}
-                    <div className="boards">
-                        {this.getBoardBtns(nonStarredBoards)}
-                    </div>
-                </div> : <></>}
+                </div> : <></>
+                }
             </div>
         )
     }
@@ -55,7 +90,8 @@ function mapStateToProps(state) {
 }
 
 const mapDispatchToProps = {
-    loadBoards
+    loadBoards,
+    updateBoard
 }
 
 export const UserBoardsPopover = connect(mapStateToProps, mapDispatchToProps)(_UserBoardsPopover)
