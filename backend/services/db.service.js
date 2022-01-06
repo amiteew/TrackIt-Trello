0a364352a68bf70ee3e51447d9e920db79b128e7
Amitee Weiner <amitweiner@google.com>
2022-01-07 00:17:58 +0200

    connect to mongoDB

--- backend/services/db.service.js
+++ backend/services/db.service.js
@@ -1,37 +1,37 @@
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient;
 
-const config = require('../config')
+const dbURL = 'mongodb+srv://mytrackitdbuser:GxsCrJr?2!@trackit.zaghh.mongodb.net/Trackit?retryWrites=true&w=majority';
 
 module.exports = {
     getCollection
-}
+};
 
 // Database Name
-const dbName = 'trackit'
+const dbName = 'Trackit';
 
-var dbConn = null
+var dbConn = null;
 
 async function getCollection(collectionName) {
     try {
-        const db = await connect()        
-        const collection = await db.collection(collectionName)
-        return collection
+        const db = await connect();
+        const collection = await db.collection(collectionName);
+        return collection;
     } catch (err) {
-        logger.error('Failed to get Mongo collection', err)
-        throw err
+        logger.error('Failed to get Mongo collection', err);
+        throw err;
     }
 }
 
 async function connect() {
-    if (dbConn) return dbConn
-    try {        
-        const client = await MongoClient.connect(config.dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
-        const db = client.db(dbName)
-        dbConn = db                
-        return db
+    if (dbConn) return dbConn;
+    try {
+        const client = await MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
+        const db = client.db(dbName);
+        dbConn = db;
+        return db;
     } catch (err) {
-        logger.error('Cannot Connect to DB', err)
-        throw err
+        logger.error('Cannot Connect to DB', err);
+        throw err;
     }
 }
 
